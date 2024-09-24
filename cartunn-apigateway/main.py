4869b1bb53afe3a485c8c10848660df63fb6891d
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-09-23 20:31:23 -0500

    feat(api gateway): update route for service tunning
    The code changes update the route for the service tunning endpoint in the API gateway. The previous route included a path parameter, but it has been modified to remove the path parameter and use a fixed route instead. This change improves the clarity and simplicity of the API endpoint.
    
    Note: The suggested commit message follows the established convention of prefixing the commit message with the type of change (feat for feature).

--- cartunn-apigateway/main.py
+++ cartunn-apigateway/main.py
@@ -6,8 +6,8 @@ app = FastAPI()
 # Microservicios URLs
 MICROSERVICE_TUNNING_URL = "http://localhost:8080/api/v1/tunning-task"
 
-@app.get("/service-tunning/{path:path}")
-async def route_to_service_tunning(path: str, request: Request):
+@app.get("/service-tunning/")
+async def route_to_service_tunning(request: Request):
     url = f"{MICROSERVICE_TUNNING_URL}"
     # Enviar la solicitud al microservicio A
     response = requests.get(url, headers=request.headers)
@@ -18,8 +18,8 @@ class TunningTask(BaseModel):
     date: str
     status: str
 
-@app.post("/service-tunning/{path:path}")
-async def route_to_service_tunning(path: str, request: Request, tunning_task: TunningTask):
+@app.post("/service-tunning/")
+async def route_to_service_tunning(request: Request, tunning_task: TunningTask):
     url = f"{MICROSERVICE_TUNNING_URL}"
     # Enviar la solicitud al microservicio A
     response = requests.post(url, headers=request.headers, json=tunning_task.dict())
